a2ebe5673c0310e4a5347e00eb9b97cc35dffb38
E.Azer Koçulu <azer@kodfabrik.com>
2014-02-06 01:23:50 -0800

    use new-command

--- bin/personal-api.js
+++ bin/personal-api.js
@@ -1,19 +1,10 @@
 #!/usr/bin/env node
 
+var command = require("new-command")({ 'p': 'port', 'o': 'hostname' });
+
 require('default-debug')('circle:server');
-
-var opt = require('optimist');
-
-var argv = opt
-      .options('port', { alias: 'p' })
-      .options('hostname', { alias: 'o' })
-      .options('version', { alias: 'v' })
-      .options('help', { alias: 'h' })
-      .argv;
-
 var server = require('../');
 
-if (argv.version) require('show-version');
-if (argv.help || argv._.length == 0) require('show-help');
-
-server.start(argv._[0], argv.port || 3000, argv.hostname);
+server.start(command._[0],
+             command.port || 3000,
+             command.hostname || '127.0.0.1');
